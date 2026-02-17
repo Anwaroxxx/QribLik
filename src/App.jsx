441280c87f98d76634b0e8fd8b0b2a521ce5cfa2
youssefelkhafif <youssefkhafif@gmail.com>
2026-02-17 18:58:39 +0000

    i add sign up and loging

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Routes>
-        
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
         <Route path="/Support" element={<Support/>}/>
         <Route path="/Signup" element={<SignUp />}/>
