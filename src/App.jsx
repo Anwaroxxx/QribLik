ff980987ca0e06e4d2cc5cb6ce883567006b3f01
Hafsa Ouahbi <hafsaouahbi52@gmail.com>
2026-02-18 12:48:00 +0000

    add black logo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home'
 import Support from './pages/Support'
 import SignUp from './components/SignUp'
 import SignIn from './components/SignIn'
+import Landing from './pages/landing'
 
 
 
@@ -13,14 +14,15 @@ function App() {
 
   return (
     <>
-      <Routes>
+    <Landing></Landing>
+      {/* <Routes>
         <Route path='*' element={<Errors/>} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
         <Route path="/Support" element={<Support/>}/>
         <Route path="/Signup" element={<SignUp />}/>
         <Route path="/SignIn" element={<SignIn />} />
-      </Routes>
+      </Routes> */}
     </>
   )
 }
