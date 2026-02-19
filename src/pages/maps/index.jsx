923a3822e6b0d1f5f08bf1706919733690349bcb
hamzaabl <hamzaabouelwahab04@gmail.com>
2026-02-19 14:03:37 +0000

    feat : add page  maps

---
+++ src/pages/maps/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DashboardMap from './partials/DashboardMap';
+import SectionMap from './partials/SectionMap';
+
+const Maps = () => {
+    return (
+        <div>
+            <DashboardMap/>
+            <SectionMap/>
+        </div>
+    );
+};
+
+export default Maps;
---
+++ src/pages/maps/partials/DashboardMap.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardMap = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DashboardMap;
